dc578e4fdbe85545d8f85dc503f3c6c1323a972e
ShauryaaSharma <shauryasofficial27@gmail.com>
2026-05-25 23:57:43 +0530

    Add WebSocket client struct and Read loop

---
+++ backend/pkg/websocket/client.go
@@ -0,0 +1,47 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	ID   string
+	Conn *websocket.Conn
+	Pool *Pool
+	mu   sync.Mutex
+}
+
+type Message struct {
+	Type int    `json:"type"`
+	Body string `json:"body"`
+}
+
+// SafeWriteJSON wraps WriteJSON with the client's mutex to prevent concurrent write races.
+func (c *Client) SafeWriteJSON(v interface{}) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.Conn.WriteJSON(v)
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		message := Message{Type: messageType, Body: string(p)}
+		c.Pool.Broadcast <- message
+		fmt.Printf("Message Received: %+v\n", message)
+	}
+}
